70f77bacd60a53418fe19f1a90a3b1767ba9a4c4
SquaredToTheFourth <97636230+SquaredToTheFourth@users.noreply.github.com>
2024-01-22 07:49:30 -0600

    Update PlayerMovement with correct max velocity checks
    The PlayerMovement script now correctly limits player velocity to the maximum

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour{
     private Rigidbody rigidBody;
     private Vector3 movementVector;
+    [SerializeField] private float speedModifier = 5f;
     [SerializeField] private Vector3 topVelocity = new Vector3(3f, 0f, 3f);
     [SerializeField] private Vector3 deceleration = new Vector3(1f, 0f, 1f);
     void Start(){
@@ -14,19 +15,25 @@ public class PlayerMovement : MonoBehaviour{
     }
 
     void Update(){
-        float xSpeed = rigidBody.velocity.x + movementVector.x / topVelocity.x;
-        if(Mathf.Abs(xSpeed) > 1) {
-            xSpeed = (topVelocity.x / xSpeed) * xSpeed;
+        float xSpeed = (rigidBody.velocity.x + movementVector.x) / topVelocity.x;
+        if (movementVector.x == 0) {
+            xSpeed = 0;
         }
-        float zSpeed = rigidBody.velocity.z + movementVector.z / topVelocity.z;
-        if (Mathf.Abs(zSpeed) > 1) {
-            zSpeed = (topVelocity.z / zSpeed) * zSpeed;
+        else if(Mathf.Abs(xSpeed) > 1) {
+            xSpeed = (1f / Mathf.Abs(xSpeed)) * xSpeed;
         }
-        rigidBody.velocity = new Vector3(xSpeed, 0f, zSpeed);
+        float zSpeed = (rigidBody.velocity.z + movementVector.z) / topVelocity.z;
+        if (movementVector.z == 0) {
+            zSpeed = 0;
+        }
+        else if (Mathf.Abs(zSpeed) > 1) {
+            zSpeed = (1f / Mathf.Abs(zSpeed)) * zSpeed;
+        }
+        rigidBody.velocity = new Vector3(xSpeed * topVelocity.x, 0f, zSpeed * topVelocity.z);
     }
 
     public void OnMove(InputValue inputValue) {
         Vector2 input = inputValue.Get<Vector2>();
-        movementVector = new Vector3(input.x * Time.deltaTime, 0, input.y * Time.deltaTime);
+        movementVector = new Vector3(input.x * Time.deltaTime * speedModifier, 0, input.y * Time.deltaTime * speedModifier);
     }
 }
