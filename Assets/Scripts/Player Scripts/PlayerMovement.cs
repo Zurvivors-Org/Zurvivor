f08fec013a1afebd1188a1177adf0bf4952a51fe
SquaredToTheFourth <97636230+SquaredToTheFourth@users.noreply.github.com>
2024-01-24 14:46:47 -0600

    Update PlayerMovement with Acceleration Calculations
    Player Movement now uses Acceleration to move the character and not Translation by Distance

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour{
-    private Rigidbody rigidBody;
     private Vector2 movementVector;
-    [SerializeField] private float speedModifier = 1f;
+    private Rigidbody rigidBody;
+    [SerializeField] private float speedModifier = 10f;
     [SerializeField] private Vector3 topVelocity = new Vector3(3f, 0f, 3f);
-    [SerializeField] private Vector3 deceleration = new Vector3(1f, 0f, 1f);
+    [SerializeField] private Vector3 topAcceleration = new Vector3(2f, 0f, 2f);
     [SerializeField] private Transform fpCamera;
     void Start(){
         rigidBody = GetComponent<Rigidbody>();
@@ -15,9 +15,11 @@ public class PlayerMovement : MonoBehaviour{
     }
 
     void FixedUpdate(){
-        movementVector.x = Mathf.Clamp(Input.GetAxis("Horizontal") * speedModifier, -topVelocity.x, topVelocity.x);
-        movementVector.y = Mathf.Clamp(Input.GetAxis("Vertical") * speedModifier, -topVelocity.z, topVelocity.z);
-        Vector3 cameraForward = new Vector3(fpCamera.forward.x, 0f, fpCamera.forward.z);
-        transform.Translate((movementVector.x * fpCamera.right + movementVector.y * cameraForward) * .02f, Space.World);
+        movementVector.x = Mathf.Clamp(Input.GetAxis("Horizontal") * speedModifier, -topAcceleration.x, topAcceleration.x);
+        movementVector.x = Mathf.Clamp(movementVector.x, -topVelocity.x - rigidBody.velocity.x, topVelocity.x - rigidBody.velocity.x);
+        movementVector.y = Mathf.Clamp(Input.GetAxis("Vertical") * speedModifier, -topAcceleration.z, topAcceleration.z);
+        movementVector.y = Mathf.Clamp(movementVector.y, -topVelocity.z - rigidBody.velocity.z, topVelocity.z - rigidBody.velocity.z);
+        transform.rotation = new Quaternion(0f, fpCamera.rotation.y, 0f, fpCamera.rotation.w);
+        rigidBody.AddForce(movementVector.x * transform.right + movementVector.y * transform.forward , ForceMode.Acceleration);
     }
 }
