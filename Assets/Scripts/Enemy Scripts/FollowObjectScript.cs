8e5fcc1a3417718f99ec566a7bedf2108694621d
SquaredToTheFourth <97636230+SquaredToTheFourth@users.noreply.github.com>
2024-02-06 14:24:57 -0600

    Add Object Follow Script that follows an object in a straight line

--- Assets/Scripts/Enemy Scripts/FollowObjectScript.cs
+++ Assets/Scripts/Enemy Scripts/FollowObjectScript.cs
@@ -7,18 +7,22 @@ public class FollowObjectScript : MonoBehaviour{
 
     public Transform objectTransform;
 
+    [Header("Movement")]
     public float movementSpeed;
+    public float drag;
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.drag = drag;
     }
 
     private void Update() {
-        Quaternion q = Quaternion.LookRotation(objectTransform.position - transform.position);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 0f);
     }
 
     private void FixedUpdate() {
+        Vector3 objectFlatPos = new Vector3(objectTransform.position.x, transform.position.y, objectTransform.position.z);
+        transform.LookAt(objectFlatPos);
         rb.AddForce(transform.forward * movementSpeed, ForceMode.Impulse);
     }
 }
