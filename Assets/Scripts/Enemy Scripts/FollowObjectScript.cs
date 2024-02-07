4dbe07ceac50a1d5d1a268022a4353c565b12495
SquaredToTheFourth <97636230+SquaredToTheFourth@users.noreply.github.com>
2024-02-07 14:32:40 -0600

    Fix Object Jittering on Collision and Add Crosshair
    Object jittering was caused by object rotation being updated in the Update method and not Fixed Update were the physics calculations take place

--- Assets/Scripts/Enemy Scripts/FollowObjectScript.cs
+++ Assets/Scripts/Enemy Scripts/FollowObjectScript.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowObjectScript : MonoBehaviour{
+    public Transform objectTransform;
     private Rigidbody rb;
 
-    public Transform objectTransform;
-
     [Header("Movement")]
-    public float movementSpeed;
+    public float moveSpeed;
     public float drag;
 
     private void Start() {
@@ -18,10 +17,22 @@ public class FollowObjectScript : MonoBehaviour{
     }
 
     private void Update() {
-        Vector3 objectFlatPos = new Vector3(objectTransform.position.x, transform.position.y, objectTransform.position.z);
-        transform.LookAt(objectFlatPos);
+        SpeedControl();
     }
     private void FixedUpdate() {
-        rb.AddForce(transform.forward * movementSpeed * Time.deltaTime, ForceMode.Impulse);
+        Vector3 objectFlatPos = new Vector3(objectTransform.position.x, transform.position.y, objectTransform.position.z);
+        transform.LookAt(objectFlatPos);
+        if (Mathf.Abs(Vector3.Magnitude(transform.position - objectTransform.position)) > 2) {
+            rb.AddForce(transform.forward * moveSpeed * 10f, ForceMode.Force);
+        }
+    }
+
+    private void SpeedControl() {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed) {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
--- Assets/Scripts/Player Scripts/CameraMovement.cs
+++ Assets/Scripts/Player Scripts/CameraMovement.cs
@@ -3,27 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour{
-    const string xMouseAxis = "Mouse X";
-    const string yMouseAxis = "Mouse Y";
-    private float xCameraLimit = 88;
-    private float xRotation = 0f;
-    private float yRotation = 0f;
     [SerializeField] private Transform orientation;
-    [SerializeField] private float cameraSensitivity = 1f;
     void Start(){
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
     void Update(){
-        float mouseX = Input.GetAxisRaw(xMouseAxis) * Time.deltaTime * cameraSensitivity;
-        float mouseY = Input.GetAxisRaw(yMouseAxis) * Time.deltaTime * cameraSensitivity;
-
-        yRotation += mouseX;
-        xRotation -= mouseY;
-
-        xRotation = Mathf.Clamp(xRotation, -xCameraLimit, xCameraLimit);
-
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
-        orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+        orientation.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
     }
 }
