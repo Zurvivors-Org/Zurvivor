17e90a78d9a4384ca7dc7dc69cf74e4208564a02
Ishaan Masil <61901829+StarkOdinson612@users.noreply.github.com>
2024-02-13 08:00:10 -0600

    Added enemy navmesh and terrain

--- Assets/Scripts/Enemy Scripts/EnemyContainer.cs
+++ Assets/Scripts/Enemy Scripts/EnemyContainer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyContainer : MonoBehaviour {
     public EnemyProperties enemyProperties;
@@ -8,16 +9,21 @@ public class EnemyContainer : MonoBehaviour {
     [Header("Enemy Properties")]
     public float health;
     public float damage;
-    public float moveSpeed;
+
+    [SerializeField] private GameObject player;
+    private NavMeshAgent agent;
+    
     void Start(){
         health = enemyProperties.health;
         damage = enemyProperties.damage;
-        moveSpeed = enemyProperties.moveSpeed;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update() {
         if(health <= 0) {
             Destroy(this.gameObject);
         }
+
+        agent.SetDestination(player.transform.position);
     }
 }
